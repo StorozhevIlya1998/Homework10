03eff3c50fd3500b7ac9dc774bb35f9dc99b2e42
Moscow-PC <storojew.i123@gmail.com>
2023-02-02 19:28:53 +0300

    task 1 is over

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+    public static void task1 () {
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println("ФИО сотрудника — " + fullName);
     }
 }
